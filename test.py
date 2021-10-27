d5ff86d1307180b8658d5b28716f00cd9e092e42
moundy-dev <69680957+moundy-dev@users.noreply.github.com>
2021-10-27 11:05:24 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+print("hello world")
+
+example_list = [1,2,4,85,78]
+string = "je m'apelle zarwa"
+
+example_list.append(44)
+for letter in string:
+    print(letter)
+
+def ma_fonction(nbr1):
+
+    print("je suis une fonction",nbr1)
+
+ma_fonction(45)
